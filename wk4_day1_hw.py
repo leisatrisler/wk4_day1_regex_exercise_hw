61630dfda7046bd75fcc1533648868a788a67e2f
leisatrisler <leisatrisler@yahoo.com>
2023-05-15 21:11:28 -0700

    updated git config name

--- wk4_day1_hw.py
+++ wk4_day1_hw.py
@@ -9,7 +9,7 @@ import re
 file1 = open('regex_test.txt', 'r')
 lines = file1.readlines()
 for l in lines:
-    match = re.match (r'^[A-Z][a-zA-Z]+\s+[A-Z][a-zA-Z]+(?!\s|$)', l) #googled unsure if this is correct this is cutting off middle inital and last letter of names but its working enough for this elvish ... I mean regex.
+    match = re.match (r'^[A-Z][a-zA-Z]+\s+[A-Z][a-zA-Z]+(?!\s|$)', l) #googled unsure if this is correct this is cutting off middle inital and last letter of names but its working enough for this elvish ... I mean regex
     if match:
         print(match.group(0))
     else:
